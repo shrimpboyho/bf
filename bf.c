8e9aa03e3197e103859c62c5ff2325c166ce235e
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-09-12 15:25:21 -0400

    Got a successful logic thingy going on here

--- bf.c
+++ bf.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main ( int argc, char* argv[] ){
 
 	FILE* s = fopen(argv[1],"r");
+	FILE* o = fopen("a.c","w");
+	fprintf(o, "#include <stdio.h>\n"
+		   "#include <stdlib.h>\n"
+			
+		   "int main( void ){\n"
+
+		    "\tprintf(\"hey\");"
+		   );
 
 	for(;;){
 
@@ -11,9 +20,26 @@ int main ( int argc, char* argv[] ){
 		if(p == EOF){
 			break;
 		}
-		printf("%c",p);
+		
 	}
+	
+	/* Close files */
+
 	fclose(s);
+
+	fprintf(o, "\treturn 0;\n"
+		   "}"
+		   );
+
+	fclose(o);
+	
+	/* Compile */
+	
+	char command[60];
+	strcat(command,"gcc a.c -o ");
+	strcat(command,argv[2]);
+	system(command);
+	remove("a.c");
 
 	return 0;
 }
